367abf2adf0ee01eff6a872e417185bacbbeea60
bbaburaj <bbaburaj@asu.edu>
2014-05-12 23:49:54 -0700

    Updated the Activity and about fragment

--- IHO-ASU Android/app/src/main/java/com/iho/asu/MainActivity.java
+++ IHO-ASU Android/app/src/main/java/com/iho/asu/MainActivity.java
@@ -1,12 +1,17 @@
 package com.iho.asu;
 
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import static android.view.View.OnClickListener;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,4 +40,17 @@ public class MainActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.about:
+                Fragment newFragment = (Fragment) new About();
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+
+                transaction.replace(R.id.about_fragment, newFragment);
+                transaction.addToBackStack(null);
+
+                transaction.commit();
+        }
+    }
 }
