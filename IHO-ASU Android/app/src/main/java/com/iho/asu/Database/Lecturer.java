bf34c07b8cec78db111da0e87688ba152336e2cb
bbaburaj <bbaburaj@asu.edu>
2014-07-04 16:29:02 -0700

    Update Database table details

--- IHO-ASU Android/app/src/main/java/com/iho/asu/Database/Lecturer.java
+++ IHO-ASU Android/app/src/main/java/com/iho/asu/Database/Lecturer.java
@@ -4,4 +4,58 @@ package com.iho.asu.Database;
  * Created by Barathi on 7/4/2014.
  */
 public class Lecturer {
+    private long id;
+    private String name;
+    private String title;
+    private String bio;
+    private String text;
+    private Object image;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Object getImage() {
+        return image;
+    }
+
+    public void setImage(Object image) {
+        this.image = image;
+    }
 }
--- IHO-ASU Android/app/src/main/java/com/iho/asu/Database/News.java
+++ IHO-ASU Android/app/src/main/java/com/iho/asu/Database/News.java
@@ -4,4 +4,49 @@ package com.iho.asu.Database;
  * Created by Barathi on 7/4/2014.
  */
 public class News {
+    private long id;
+    private String title;
+    private String text;
+    private Object image;
+    private String newsLink;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Object getImage() {
+        return image;
+    }
+
+    public void setImage(Object image) {
+        this.image = image;
+    }
+
+    public String getNewsLink() {
+        return newsLink;
+    }
+
+    public void setNewsLink(String newsLink) {
+        this.newsLink = newsLink;
+    }
 }
