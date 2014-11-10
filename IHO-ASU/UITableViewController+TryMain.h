08e1fb670a95e9d4c44c5091398b118d26c9f0d0
Tanmaya Nelapatla <tanela16@gmail.com>
2014-11-10 16:11:08 -0700

    iPhone done

--- IHO-ASU/UITableViewController+TryMain.h
+++ IHO-ASU/UITableViewController+TryMain.h
@@ -11,7 +11,15 @@
 @interface TryMain:UITableViewController
 
 @property (weak, nonatomic) IBOutlet UIButton *connect;
+@property (weak, nonatomic) IBOutlet UIButton *news;
+@property (weak, nonatomic) IBOutlet UIButton *gallery;
+@property (weak, nonatomic) IBOutlet UIButton *about;
+@property (weak, nonatomic) IBOutlet UIButton *donate;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *customItem1;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *coR;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *credits;
 
+@property (weak, nonatomic) IBOutlet UIButton *field;
 @property (weak, nonatomic) IBOutlet UIWebView *ihologo;
 
 @end
